113069b5b716d9e63ca5167c009c6d6e8e47ba19
Jess Varghese <jessvarghese@gmail.com>
2021-12-15 15:59:28 -0500

    updated files

--- models/index.js
+++ models/index.js
@@ -12,7 +12,6 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-
 User.belongsToMany(Post, {
   through: Vote,
   as: 'voted_posts',
@@ -41,8 +40,4 @@ Post.hasMany(Vote, {
   foreignKey: 'post_id'
 });
 
-
-
 module.exports = { User, Post, Vote };
-
-
